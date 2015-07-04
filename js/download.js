86604f064684f9ac3a19639fa090f987edf602c3
Frederic <guydesmonts@gmail.com>
2015-07-04 18:19:23 -0400

    Changed approach to downloading the svgi, now with its own function and .js file

---
+++ js/download.js
@@ -0,0 +1,17 @@
+function download(filename, text) {
+        var pom = document.createElement('a');
+            pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+                pom.setAttribute('download', filename);
+
+                if (document.createEvent) {
+                            var event = document.createEvent('MouseEvents');
+                                    event.initEvent('click', true, true);
+                                            pom.dispatchEvent(event);
+
+                }
+                else {
+                            pom.click();
+
+                }
+
+}
