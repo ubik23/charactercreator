ed9765f76cf6d5ad8702ed1a10ad4f30a0b543ef
Frederic <guydesmonts@gmail.com>
2015-07-05 09:10:44 -0400

    First export of SVG content. Some elements appear in Inkscape, most don't. Still needs work.

--- js/download.js
+++ js/download.js
@@ -28,20 +28,21 @@ function download(filename = 'my_character.svg') {
                 }
             }
         });
-        console.log(text);
-        //var pom = document.createElement('a');
-            //pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-                //pom.setAttribute('download', filename);
+        text += '</svg>';
+        //console.log(text);
+        var pom = document.createElement('a');
+            pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+                pom.setAttribute('download', filename);
 
-                //if (document.createEvent) {
-                            //var event = document.createEvent('MouseEvents');
-                                    //event.initEvent('click', true, true);
-                                            //pom.dispatchEvent(event);
+                if (document.createEvent) {
+                            var event = document.createEvent('MouseEvents');
+                                    event.initEvent('click', true, true);
+                                            pom.dispatchEvent(event);
 
-                //}
-                //else {
-                            //pom.click();
+                }
+                else {
+                            pom.click();
 
-                //}
+                }
 
 }
