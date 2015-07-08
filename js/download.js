e0f61c5d20aa98d80fea143692ef1f34f8a41206
Frederic <guydesmonts@gmail.com>
2015-07-07 20:47:32 -0400

    Completed download SVG function.

--- js/download.js
+++ js/download.js
@@ -1,33 +1,30 @@
 function download(filename = 'my_character.svg') {
         //var text = document.getElementById('svg1').innerHTML;
         //var text = text || '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n<!-- Created with Inkscape (http://www.inkscape.org/) -->\n<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.1" width="560" height="560" id="character">\n';
-        var text = '<svg xmlns="http://www.w3.org/2000/svg" id="character" width="560" height="560">'
+        var text = '<!-- ?xml version="1.0" encoding="UTF-8" standalone="no"? -->\n<svg xmlns="http://www.w3.org/2000/svg" id="character" width="560" height="560">\n'
         var svgRaw = document.getElementById('svg1').childNodes;
         //This previous version of the text contains all svg files shown and hidden
         //It will need to be filtered to keep only the layers needed for our purpose
         var svgNodes = Array.prototype.slice.call(svgRaw);
-
         svgNodes.forEach(function(item){
+            //console.log(item.style);
+            //This is where we start filtering the nodes so that we can append themm into our downloaded file.
+            //
             if (item.style != undefined){
+                //This removes only useless layers and allows us to o the next test.
                 if (item.style.opacity != 0){
-                    //console.log(item.childNodes);
-                    //text.concat(item.childNodes.innerHTML);
-                    var svgArray = Array.prototype.slice.call(item.childNodes);
-                    for (var el in svgArray) {
-                        //console.log(text);
-                        //console.log(svgArray[el].innerHTML);
-                        var newString = svgArray[el].innerHTML;
-                        if (newString != undefined && newString != "" && newString != "Created with Snap"){
-                            newString = newString.trim();
-                            if (newString.slice(0,9) != "<rdf:rdf>"){
-                                text += newString;
-                            }
-                        }
-                    }
+                    //console.log( item);
+                    console.log(item.innerHTML);
+                    var svgString = item.innerHTML;
+                    console.log(svgString.slice(-43));
+                    if (svgString.slice(-43) === "<desc>Created with Snap</desc><defs></defs>"){
+                        svgString = svgString.slice(0, -43);
+                    };
+                    text += svgString;
                 } else {
-                    //console.log('undefined', item)
-                }
-            }
+                };
+            } else {
+            };
         });
         text += '</svg>';
         //console.log(text);
