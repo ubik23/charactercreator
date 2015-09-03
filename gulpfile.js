fffccb729b60db342f04891657b44cbea0839486
Frederic <guydesmonts@gmail.com>
2015-09-02 23:53:40 -0400

    Modified gulpfile to concat and minify css and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['sass']);
 });
 
 gulp.task('scripts', function() {
@@ -10,3 +12,22 @@ gulp.task('scripts', function() {
     .pipe(concat('all.js'))
     .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'));
+
+});
+
+gulp.task('sass', function () {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./'));
+});
+
+
+
+
