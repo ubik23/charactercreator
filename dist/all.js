d4bf5b0b1cef7c6d0b573d374415476b9dd3724c
Frederic <guydesmonts@gmail.com>
2015-10-24 20:55:58 -0400

    Add loadScript funtction, wip.

--- dist/all.js
+++ dist/all.js
@@ -440,7 +440,26 @@ function createForm(sex, forms){
         var htmlObject = document.createElement('div');
         htmlObject.innerHTML = newHtml;
         formContainer.appendChild(htmlObject);
+        loadScript('lib/jscolor/jscolor.js');
     }
+}
+
+
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    //script.onreadystatechange = callback;
+    //script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
 }
 
 
