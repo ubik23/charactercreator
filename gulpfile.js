002973fdd98e6556de360e44a52bb1a6a1dbc47c
Frederic <guydesmonts@gmail.com>
2015-11-08 13:57:44 -0500

    Fix gulp settings.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var autoprefixer = require('autoprefixer');
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
 
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['sass']);
@@ -25,18 +27,15 @@ gulp.task('sass', function () {
         .pipe(sass())
         //.pipe(gulp.dest('css'))
         .pipe(concat('styles.css'))
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('autoprefixer', function () {
-    var postcss      = require('gulp-postcss');
-    var sourcemaps   = require('gulp-sourcemaps');
-    var autoprefixer = require('autoprefixer');
 
     return gulp.src('./css/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dest'));
 });
 
