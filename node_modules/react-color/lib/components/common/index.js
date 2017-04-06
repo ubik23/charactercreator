8ce6e471a83715a69cf4493d641d85b5009ecdab
Frederic <guydesmonts@gmail.com>
2017-04-06 11:06:33 -0400

    remove node modules

--- node_modules/react-color/lib/components/common/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  Alpha: require('./Alpha'),
-  Checkboard: require('./Checkboard'),
-  EditableInput: require('./EditableInput'),
-  Hue: require('./Hue'),
-  Saturation: require('./Saturation')
-};
--- node_modules/react-color/modules/react-material-design/index.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  Raised: require('./lib/components/Raised'),
-  Tile: require('./lib/components/Tile'),
-
-  Tabs: require('./lib/components/Tabs'),
-};
