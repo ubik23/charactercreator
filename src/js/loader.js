903f882a12caa2e664d5682e83e5a70001686cb4
Frederic <guydesmonts@gmail.com>
2018-05-28 17:55:24 -0400

    Remove call to Snap.

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,29 +1,29 @@
-
-Snap.plugin( function( Snap, Element, Paper, global ) {
-    function addLoadedFrags( whichSVG, fragList, runWhenFinishedFunc ) { // This is called once all the loaded frags are complete
-        for( var count = 0; count < fragList.length; count++ ) {
-        myEl = whichSVG.append( fragList[ count ] );
-        }
-        runWhenFinishedFunc();
-    }
-    Paper.prototype.loadFilesDisplayOrdered = function( list, afterAllLoadedFunc, onEachElementLoadFunc ) {
-        var image, fragLoadedCount = 0, listLength = list.length, fragList = new Array(), whichSVG = this;
-        for( var count = 0; count < listLength; count++ ) {
-            (function() {
-                var whichEl = count,
-                fileName = list[ whichEl ]+'.svg',
-                image = Snap.load( fileName, function ( loadedFragment ) {
-                    fragLoadedCount++;
-                    onEachElementLoadFunc( loadedFragment, fileName );
-                    fragList[ whichEl ] = loadedFragment;
-                    if( fragLoadedCount >= listLength ) {
-                        addLoadedFrags( whichSVG, fragList, afterAllLoadedFunc );
-                    }
-                });
-            })();
-        }
-    };
-});
+// 
+// Snap.plugin( function( Snap, Element, Paper, global ) {
+//     function addLoadedFrags( whichSVG, fragList, runWhenFinishedFunc ) { // This is called once all the loaded frags are complete
+//         for( var count = 0; count < fragList.length; count++ ) {
+//         myEl = whichSVG.append( fragList[ count ] );
+//         }
+//         runWhenFinishedFunc();
+//     }
+//     Paper.prototype.loadFilesDisplayOrdered = function( list, afterAllLoadedFunc, onEachElementLoadFunc ) {
+//         var image, fragLoadedCount = 0, listLength = list.length, fragList = new Array(), whichSVG = this;
+//         for( var count = 0; count < listLength; count++ ) {
+//             (function() {
+//                 var whichEl = count,
+//                 fileName = list[ whichEl ]+'.svg',
+//                 image = Snap.load( fileName, function ( loadedFragment ) {
+//                     fragLoadedCount++;
+//                     onEachElementLoadFunc( loadedFragment, fileName );
+//                     fragList[ whichEl ] = loadedFragment;
+//                     if( fragLoadedCount >= listLength ) {
+//                         addLoadedFrags( whichSVG, fragList, afterAllLoadedFunc );
+//                     }
+//                 });
+//             })();
+//         }
+//     };
+// });
 
 // it uses fragments, so they aren't loaded yet into the DOM fully
 function onAllLoaded() {
