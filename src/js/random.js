4c857f9a255edcea7e70f4fedfcca3be5c6a48ee
Frederic <guydesmonts@gmail.com>
2019-12-11 12:32:07 -0500

    Add analytics to WIP random function.

--- src/js/random.js
+++ src/js/random.js
@@ -159,6 +159,7 @@ function smartRandomStream() {
 }
 
 function smartRandomSingle() {
+  ga('send', 'event', { eventCategory: 'Secret function', eventAction: 'smartRandomSingle()', eventLabel: 'Secret Patreon reveal for new random function.'});
   var roll;
   var skinTones = ['#FFDFC4', '#F0D5BE', '#EECEB3', '#E1B899', '#E5C298', '#FFDCB2', '#E5B887', '#E5A073', '#E79E6D', '#DB9065', '#CE967C', '#C67856', '#BA6C49', '#A57257', '#F0C8C9', '#DDA8A0', '#B97C6D', '#A8756C', '#AD6452', '#5C3836', '#CB8442', '#BD723C', '#704139', '#A3866A']
   var obj = new Array();
@@ -222,6 +223,7 @@ function smartRandomSingle() {
   defaultPupilShape();
 
   setTimeout(function() {
+
       forms = window.forms;
       console.log('forms', forms);
       counter = formLen = forms.length;
