5eb102cb3aac6bf0e74387203f4fa1961005eb65
Robin Millette <robin@millette.info>
2020-07-21 08:13:06 -0400

    Add configuration sample.

---
+++ config-sample.js
@@ -0,0 +1,15 @@
+{
+	"title": "The Character Creator",
+	"subtitle": "Build visually stunning avatars",
+	"description": "Create realistic characters for fun or for use in creative projects. Choose from a series of options to build your full body avatar in vector graphics.",
+	"ogImage": "https://charactercreator.org/character-creator-og-image.png",
+	"ogUrl": "https://charactercreator.org/",
+	"": "",
+	"": "",
+	"": "",
+	"": "",
+	"": "",
+	"": "",
+	"": "",
+	"": ""
+}
