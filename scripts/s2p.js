c862200462cb5b08e8a6590404d55eddabfc82f7
Robin Millette <robin@millette.info>
2020-07-24 14:02:01 -0400

    Add download pdf option to server-side.

--- scripts/s2p.js
+++ scripts/s2p.js
@@ -1,11 +1,24 @@
 'use strict'
 
+// npm
+const PDFDocument = require('pdfkit')
+const s2pdf = require("svg-to-pdfkit")
 const sharp = require('sharp')
 const fastify = require('fastify')({ logger: true })
 
-fastify.post('/', async (request, reply) => {
+fastify.post('/png', async (request, reply) => {
   reply.type("image/png")
   return sharp(Buffer.from(request.body.svg)).png().toBuffer()
+})
+
+fastify.post('/pdf', async (request, reply) => {
+  const doc = new PDFDocument()
+  s2pdf(doc, request.body.svg, 0, 0)
+  doc.on('pageAdded', () => doc.text("More content, page 2??"))
+  doc.addPage()
+  reply.type("application/pdf")
+  doc.end()
+  return doc
 })
 
 fastify.listen(3000, (err, address) => {
