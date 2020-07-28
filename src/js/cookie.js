d160a5a1ce3547d86a4377cb336b74a654fd6d1d
Robin Millette <robin@millette.info>
2020-07-27 22:34:46 -0400

    Parse form cookie, client-side.

---
+++ src/js/cookie.js
@@ -0,0 +1,21 @@
+/*!
+ * cookie
+ * Copyright(c) 2012-2014 Roman Shtylman
+ * Copyright(c) 2015 Douglas Christopher Wilson
+ * MIT Licensed
+ */
+
+'use strict'
+
+function parseFormCookie() {
+  const pairs = document.cookie.split(/; */)
+  for (var i = 0; i < pairs.length; i++) {
+    const pair = pairs[i]
+    const eq_idx = pair.indexOf('=')
+    if ((eq_idx < 0) || (pair.substr(0, eq_idx).trim() !== "form")) continue
+    var val = pair.substr(++eq_idx, pair.length).trim()
+    if ('"' == val[0]) val = val.slice(1, -1)
+    return JSON.parse(decodeURIComponent(val).split(".").slice(0, -1).join("."))
+  }
+  return {}
+}
