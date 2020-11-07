41cdc9602188abbc65e3cbbf1bbe2cffc812abaa
ubik <guydesmonts@gmail.com>
2020-11-07 17:26:13 -0500

    Add six-button asymettric vest.

--- src/js/globals.js
+++ src/js/globals.js
@@ -26,7 +26,7 @@ var maleForm2 = {
 var maleForm3 = {
   Shirt: ['', 'tanktop', 'colar', 'kurta', 'tshirt', 'turtleneck'],
   Tie: ['', 'neck', 'bolo', 'bow'],
-  Vest: ['', 'vest', 'lined', 'insulated', 'mandarin', 'u-neck', 'yellow', '3-button_asymmetric'],
+  Vest: ['', 'vest', 'lined', 'insulated', 'mandarin', 'u-neck', 'yellow', '3-button_asymmetric', '6-button_asymmetric'],
   Button: ['', 'heart', 'peace', 'smile'],
   Holster: ['', 'revolver_chest', 'revolver_hip'],
   Shoulderpads: ['', 'artillery', 'general', 'plated', 'spikes'],
