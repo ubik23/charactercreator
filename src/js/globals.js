19e8d46348e28aa7f0ae5356ae8fcb4ac357d867
ubik <guydesmonts@gmail.com>
2021-05-21 12:54:46 -0400

    Add collar shirt to female template.

--- src/js/globals.js
+++ src/js/globals.js
@@ -86,7 +86,7 @@ var femaleForm3 = {
   Necklace: ['', 'dogtags', 'heart', 'perl', 'princess', 'stethoscope', 'squared-circle', 'wooden'],
   Bra: ['', 'bow', 'grid', 'sports'],
   Top: ['', 'asymetric', 'loop', 'tank', 'tube_v'],
-  Shirt: ['', 'sweat'],
+  Shirt: ['', 'collar', 'sweat'],
   Button: ['', 'heart', 'peace', 'smile'],
   Shoulderpads: ['', 'artillery', 'general', 'plated', 'spikes'],
   Scarf: ['', 'chest_warmer', 'parisian_knot', 'twice_around', 'four_in_hand', 'reverse_drape_cross', 'reverse_drape_tuck', 'fake_knot', 'reverse_drape', 'overhand', 'once_around', 'drape']
