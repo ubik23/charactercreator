cd51180f5b3f8004673fe9d92e7f584bf25d3a12
Robin Millette <robin@millette.info>
2022-03-13 16:19:05 -0400

    Remove unused files.

--- cypress/integration/sample_spec.js
+++
@@ -1,8 +0,0 @@
-describe('My First Test', () => {
-  it('click Random', () => {
-    cy.visit('http://localhost:5000/')
-    cy.get('#bigRedButton').click()
-    cy.url().should('include', '#sex=')
-    cy.url().should('include', '&skinColor=')
-  })
-})
