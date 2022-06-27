f1e8ebd698edb81efe8e13ff547a64831adcb09b
ubik <guydesmonts@gmail.com>
2022-06-27 19:26:56 -0400

    WIP, raf animation works when increasing size

--- src/js/resize-head.js
+++ src/js/resize-head.js
@@ -11,64 +11,81 @@ function resizeHead (ev) {
 }
 
 function applyHeadResize (headSize) {
-    console.log('>>>currentHeadSize', currentHeadSize)
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
+    var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame
+    var start = Date.now()
+    console.log('applyHeadResize>>>currentHeadSize', currentHeadSize)
     const animDur = 250 // Duration in milliseconds for the changing head size animation
-    let start, previousTimeStamp
+    let previousTimeStamp
     let done = false
+    var myReq
 
     if (headSize === 0) {
         horizontalCompensation = 0
         verticalCompensation = 0
     }
 
+
     function stepHeadResize(timestamp) {
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>stepHeadResize')
+        const elapsed = Date.now() - start
         const horizontalOffset = -28.17
         const verticalOffset = -15.5
-        let headElements, counter, scaleValue, horizontalCompensation, verticalCompensation, animHeadSize
+        let counter, scaleValue, horizontalCompensation, verticalCompensation, animHeadSize
+        let headElements = document.querySelectorAll('#svg1 .js-head-element')
+
+        // if (start === undefined) {
+        //     console.log('!!!!!!! START !!!!!!!!')
+        //     start = timestamp
+        // }
+
+        // animHeadSize = currentHeadSize + (headSize - currentHeadSize * (elapsed / animDur))
+        animHeadSize = Number(currentHeadSize) + (elapsed / animDur)
+        horizontalCompensation = (animHeadSize * horizontalOffset)
+        verticalCompensation = (animHeadSize * verticalOffset)
+        scaleValue = 1 + (animHeadSize / 10)
+
+        // console.log('start', start)
+        // console.log('timestamp', Date.now())
+        // console.log('elapsed', elapsed)
+        // console.log('animDur', animDur)
+        console.log('elapsed / animDur', (elapsed / animDur))
+        // console.log('headSize', headSize)
+        console.log('currentHeadSize', currentHeadSize)
+        // console.log('headSize - currentHeadSize', headSize - currentHeadSize)
+        console.log('animHeadSize', animHeadSize)
+        // console.log('scaleValue', scaleValue)
         
 
-        if (start === undefined) {
-            start = timestamp
+        // Math.min() is used here to make sure the element stops at exactly 200px
+    //     const count = Math.min(0.1 * elapsed, 200);
+    //     element.style.transform = 'translateX(' + count + 'px)';
+    //     if (count === 200) done = true;
+        counter = headElements.length
+
+        while(counter--) {
+            // animHeadSize = (currentHeadSize + ((headSize - currentHeadSize) * (elapsed / animDur))) / 10
+            // n = 0 + ((0 - 1 ( * (125 / 250)))/10)
+
+            headElements[counter].setAttribute('transform',  'matrix(' + scaleValue + ',0,0,' + scaleValue + ',' + horizontalCompensation + ',' + verticalCompensation + ')')
         }
 
-        const elapsed = timestamp - start
-
-        if (previousTimeStamp !== timestamp) {
-            console.log('elapsed', elapsed)
-            headElements = document.querySelectorAll('#svg1 .js-head-element')
-            counter = headElements.length
-            
-
-            // Math.min() is used here to make sure the element stops at exactly 200px
-        //     const count = Math.min(0.1 * elapsed, 200);
-        //     element.style.transform = 'translateX(' + count + 'px)';
-        //     if (count === 200) done = true;
-
-            while(counter--) {
-                // animHeadSize = (currentHeadSize + ((headSize - currentHeadSize) * (elapsed / animDur))) / 10
-                // n = 0 + ((0 - 1 ( * (125 / 250)))/10)
-                console.log('currentHeadSize', currentHeadSize)
-                animHeadSize = currentHeadSize + ((headSize - currentHeadSize * (elapsed / animDur)))
-                console.log('animHeadSize', animHeadSize)
-                scaleValue = 1 + (animHeadSize / 10)
-                console.log('scaleValue', scaleValue)
-                horizontalCompensation = (animHeadSize * horizontalOffset)
-                verticalCompensation = (animHeadSize * verticalOffset)
-
-                headElements[counter].setAttribute('transform',  'matrix(' + scaleValue + ',0,0,' + scaleValue + ',' + horizontalCompensation + ',' + verticalCompensation + ')')
-            }
-
-          }
         
-          if (elapsed < animDur) { // Stop the animation after the set duration
-            previousTimeStamp = timestamp
-            !done && window.requestAnimationFrame(stepHeadResize);
-          }
+        if (elapsed < animDur) { // Stop the animation after the set duration
+        // previousTimeStamp = timestamp
+        // !done && window.requestAnimationFrame(stepHeadResize)
+        // console.log('!done')
+        requestAnimationFrame(stepHeadResize)
+        } else {
+        cancelAnimationFrame(stepHeadResize)
+        }
     }
 
-    window.requestAnimationFrame(stepHeadResize);
+    requestAnimationFrame(stepHeadResize)
 
-    // currentHeadSize = headSize
+    // cancelAnimationFrame(myReq)
+    currentHeadSize = headSize
+    console.log('End animation, currentHeadSize is now ', currentHeadSize)
     ///////////////////////////////////////////////////////////////////////
 
 }
