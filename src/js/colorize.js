2655b8c01bd910af94bb9506a782747e56ad4d66
ubik <guydesmonts@gmail.com>
2022-06-28 13:21:29 -0400

    Clean code on shadeColor function, moved concatenated lines of code on their own line

--- src/js/colorize.js
+++ src/js/colorize.js
@@ -1,5 +1,7 @@
 function shadeColor (color, percent) {
-  var num = parseInt(color.slice(1), 16); var amt = Math.round(2.55 * percent); var R = (num >> 16) + amt; var G = (num >> 8 & 0x00FF) + amt; var B = (num & 0x0000FF) + amt
+  var num = parseInt(color.slice(1), 16)
+  var amt = Math.round(2.55 * percent); var R = (num >> 16) + amt
+  var G = (num >> 8 & 0x00FF) + amt; var B = (num & 0x0000FF) + amt
   return '#' + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1)
 }
 
