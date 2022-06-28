8c7fc20056c31ed47cc92d79f5d4b7f8ed1888fb
ubik <guydesmonts@gmail.com>
2022-06-28 14:22:52 -0400

    Clean code, add spacing after if statements, loops, etc

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,14 +1,18 @@
 // Change sex of character from male to female or vice versa.
 function trans (sex) {
   if (c.choices.sex === sex) { return }
+
   var characterSVG = document.querySelector('#svg1 .character-container')
+
   characterSVG.classList.add('character--hide')
   hash.add({ sex: sex })
   hash.add({ emotion: 'neutral' }) // Female and Male templates have different set of emotions at this time.
   // ^ Should really check to see if the emotion doesn't exist before forcing a change to neutral.
+
   if (currentUser && currentUser.cc && currentUser.cc.personnages && currentUser.personnageActuel) {
     currentUser.cc.personnages[personnageActuel].sex = sex
   }
+
   window.sex = sex
   buildCharacter(resetForms)
 }
@@ -16,6 +20,7 @@ function trans (sex) {
 // TODO change setTimeout to promises.
 function buildCharacter (callback) {
   var characterSVG = document.querySelector('#svg1')
+
   setTimeout(function () {
     zoomFull()
     clearForms()
@@ -56,47 +61,56 @@ function Character (choices) {
     underwear: 'plain', // or random, whatever.
     underwearColor: '#f2f2f2' // Or random from a list of fabrics',
   }
+
   this.choices.emotion = this.choices.emotion || 'neutral'
   this.choices.body = this.choices.body || 'default'
   // this.choices.lips = this.choices.lips || 'default';
+
   if (this.skinTone) {
     this.choices.skinColor = this.skinTone
   }
+
   this.choices.hairColor = this.choices.hairColor || '#ffe680'
   this.choices.irisColor = this.choices.irisColor || '#2ad4ff'
   this.choices.underwear = this.choices.underwear || 'plain'
   this.choices.underwearColor = this.choices.underwearColor || '#f2f2f2'
 
   choices = this.choices
+
   if (!choices.body_head) {
     choices.body_head = 'default'
   }
+
   if (!choices.ears) {
     choices.ears = 'default'
   }
+
   if (!choices.nose) {
     choices.nose = 'default'
   }
-};
+}
 
 function modCharacter (myKey, myValue) {
   // look in c.choices to see if the key is already there
   if (myKey in c.choices) {
     delete c.choices[myKey]
-  };
+  }
+
   if (myKey === 'brows' || myKey === 'eyes' || myKey === 'lashes' || myKey === 'sockets') {
     return
   }
+
   // If there, modify the value
   // if not, add it in, with the value
   // if the value is '', then delete the key from the object,
   if (myValue != '') {
     c.choices[myKey] = myValue
-  };
+  }
+
   if (currentUser && currentUser.cc && currentUser.cc.personnages && currentUser.cc.personnageActuel) {
     currentUser.cc.personnages[currentUser.cc.personnageActuel][myKey] = myValue
   }
-};
+}
 
 function createCharacter () {
   document.getElementById(sex + 'Button').checked = true
@@ -104,11 +118,13 @@ function createCharacter () {
   // Draw stuff from the hash
   var forms = [form1, form2, form3]
   var layerCount
+
   for (var lot in forms) {
     for (var x in forms[lot]) {
       var sectionTitle = x
       var t = sectionTitle.toLowerCase()
       var xsel = hash.get(t)
+
       if (xsel !== undefined) {
         var id = '#' + t + '_' + xsel
         layerCount = isInMultiLayerArray(id.slice(1), multiLayer)
@@ -131,14 +147,17 @@ function createCharacter () {
 
 function GetEmotionGetLayers (option) {
   var faceElements = ['brows', 'eyes', 'mouth', 'lashes', 'sockets']
+
   return faceElements
 };
 
 function getOptionsLogic (section) {
   var forms = window.forms
   var section = capitalizeFirstLetter(section)
+
   for (i in forms) {
     options = forms[i][section]
+    
     if (options != undefined) {
       return options
     }
@@ -147,17 +166,22 @@ function getOptionsLogic (section) {
 
 function show (userChoice, category) {
   var multiLayer = getMultiLayer()
+
   showTutorial('colors')
+
   if (typeof (category) === 'string') {
     var sections = [category]
   } else {
     var sections = [category.split(' ')[1]]
-  };
+  }
+
   var selectedOption = userChoice
   var options = getOptionsLogic(sections[0])
   var obj = new Array()
   var id = '#' + sections[0] + '_' + selectedOption
+
   obj[category] = userChoice
+  
   if (userChoice === '') {
     modCharacter(category, userChoice)
     hash.remove(category)
@@ -171,20 +195,26 @@ function show (userChoice, category) {
   if (sections[0] === 'emotion') {
     modCharacter(sections[0], selectedOption)
     gaga('send', 'event', 'menu', 'select', id)
+
     sections = [] // Reset the sections layer so it doesn't contain 'emotion', as it isn't a layer in itself.
     var emotions = GetEmotionGetLayers(selectedOption)
+
     for (emo in emotions) {
       var newEmo = emotions[emo]
+
       sections.push(newEmo)
     }
-  };
+  }
+
   displaySections(sections, options, selectedOption, multiLayer)
 }
 
 function displaySections (sections, options, selectedOption, multiLayer) {
+
   for (section in sections) {
     options.forEach(function (d, i) {
       var id = '#' + sections[section] + '_' + d
+
       if (selectedOption != '' && d === selectedOption) {
         sectionShow(multiLayer, id)
 
@@ -197,6 +227,7 @@ function displaySections (sections, options, selectedOption, multiLayer) {
           modCharacter(sections[section], selectedOption)
           gaga('send', 'event', 'menu', 'select', id)
         }
+
       } else {
         for (lyr in multiLayer) {
           sectionHide(multiLayer, id)
@@ -229,7 +260,9 @@ function sectionShow (multiLayer, id) {
       svgContainer.querySelector(idOf).style.opacity = 1
       svgContainer.querySelector(idOf).style.pointerEvents = 'auto'
     }
+
   } else {
+
     if (layerCount > 0) {
       for (var i = 1; i <= layerCount; i++) {
         idOf = id + '_' + i + '_of_' + layerCount
@@ -244,9 +277,9 @@ function sectionShow (multiLayer, id) {
       svgContainer.querySelector(id).style.pointerEvents = 'auto'
     }
   }
-  // console.log('eyes?', id.slice(1).split('_')[0])
+  
   if (id.slice(1).split('_')[0] === 'eyes') {
-    // changeClipPathOnEyes(id)
+    
     applyClipPath()
   }
 }
@@ -275,6 +308,7 @@ function getAllBodyLayers () {
     bodyParts = getBodyLayers(layers[counter])
     files = files.concat(bodyParts)
   }
+
   return files
 }
 
@@ -298,6 +332,7 @@ function sectionHide (multiLayer, id) {
   var svgContainer = document.querySelector('#svg1')
   var sectionToHide
   var layerCount = isInMultiLayerArray(id.slice(1), multiLayer)
+
   if (layerCount > 0) {
     for (var i = 1; i <= layerCount; i++) {
       idOf = id + '_' + i + '_of_' + layerCount
@@ -325,5 +360,5 @@ function sectionHide (multiLayer, id) {
       sectionToHide.style.opacity = 0
       sectionToHide.style.pointerEvents = 'none'
     }
-  };
+  }
 }
